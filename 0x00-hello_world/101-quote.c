64477ceb7fc7c3d6f47edc14e6185bc790f7baf1
ruovagems <kelechukwufavour.faye@gmail.com>
2023-03-17 16:32:43 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - print a c program using puts
- * Return: 1
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

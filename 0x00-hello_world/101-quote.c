2aa5c454c39e5ff04e33f3dff66912c550d2f514
ruovagems <kelechukwufavour.faye@gmail.com>
2023-03-17 16:26:57 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - print a c program using puts
+ * Return: 1
+*/
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

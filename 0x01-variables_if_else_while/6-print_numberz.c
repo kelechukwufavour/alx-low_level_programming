807ceb1017b2daddbf60be61ba8a8cfce7c68ed7
ruovagems <kelechukwufavour.faye@gmail.com>
2023-03-18 09:15:24 -0700

    tasks update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar ((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}

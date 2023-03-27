622962455b7c192fac16ab4cff8523310d667f8a
ruovagems <kelechukwufavour.faye@gmail.com>
2023-03-27 15:53:21 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets value of pointer to 98
+ * @n: int to update
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Pointers are used to store address of anotber variable in C
+Pointers are also called Memory location

318b4634229ca82840e1e2b0bde3bcf4c998b2e9
Ruovagems <kelechukwufavour.faye@gmail.com>
2023-08-23 15:47:57 -0700

    tasks

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* div - divides two given integers
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of the two integers
+*/
+int div(int a, int b)
+{
+int ans;
+ans = a / b;
+return (ans);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - miltiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

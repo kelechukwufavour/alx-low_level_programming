f9caea62fb06d3001a0eb6e286092aeb665d7981
Ruovagems <kelechukwufavour.faye@gmail.com>
2023-08-23 05:53:02 -0700

    corrections and more

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,27 +8,27 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    dlistint_t *temp = *head;
-    
-    if (new_node == NULL)
-        return NULL;
-    
-    new_node->n = n;
-    new_node->next = NULL;
-    
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return new_node;
-    }
-    
-    while (temp->next != NULL)
-        temp = temp->next;
-    
-    new_node->prev = temp;
-    temp->next = new_node;
-    
-    return new_node;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *temp = *head;
+
+	if (new_node == NULL)
+		return NULL;
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return new_node;
+	}
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	new_node->prev = temp;
+	temp->next = new_node;
+
+	return new_node;
 }

508ee44cb1d635c47cf19aecf686f0697e982357
Ruovagems <kelechukwufavour.faye@gmail.com>
2023-08-23 06:17:07 -0700

    task corrections

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = NULL;
@@ -22,5 +22,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	*head = new_node;
 
-	return new_node;
+	return (new_node);
 }
